8fab298b3407c8344ddf423bfb585abe934e30d8
Tsuneki Umeda <umedat@amazon.co.jp>
2020-11-25 20:43:22 +0900

    Prism config

--- src/components/Complete/Example.js
+++ src/components/Complete/Example.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
-import theme from 'prism-react-renderer/themes/vsDark'
+import theme from 'prism-react-renderer/themes/github'
 
-const exampleCode = `
-(function someDemo() {
-  var test = "Hello World!";
-  console.log(test);
-})();`
+const Example = props => {
+  const className = props.children.props.className
+  const language = className.replace(/language-/, '')
 
-const Example = () => {
   return (
     <div>
-      <Highlight {...defaultProps} code={exampleCode} language="jsx">
+      <Highlight
+        {...defaultProps}
+        code={props.children.props.children.trim()}
+        language={language}
+        theme={theme}
+      >
         {({ className, style, tokens, getLineProps, getTokenProps }) => (
           <pre className={className} style={style}>
             {tokens.map((line, i) => (
